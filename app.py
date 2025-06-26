8941c6c03320ecd9e41823dcf0c957659aa19a9c
kwestra <kk.westra@student.han.nl>
2025-06-26 16:41:49 +0200

    Created get_json function. Gets the terms and definitions from a file.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 
 def dump_json(contents: dict, filename: str):
-    """ Dumps the current terms and definitions into a file
+    """Dumps the current terms and definitions into a file
 
     Args:
         contents: dict, contains the current terms and definitions.
@@ -15,7 +15,23 @@ def dump_json(contents: dict, filename: str):
         json.dump(contents, file)
         print(f"You did it, {filename} has been updated succesfully!")
 
-test_contents = {"Term1": "Def1",
-                 "Term2": "Def2"}
+def get_json(filename: str) -> dict:
+    """Gets the terms and definitions from a file.
+
+    Args:
+        filename: str, contains the name of the file the terms and
+                       definitions will be placed into.
+
+    Returns:
+        dict, contains the current terms and definitions.
+    """
+    if not filename.endswith(".json"):
+        pass
+    with open(filename, "r", encoding="utf-8") as file:
+        contents = json.load(file)
+        print(f"You did it, successfully loaded {filename}!")
+    return contents
+
 test_file = "test_td.json"
+test_contents = get_json(test_file)
 dump_json(test_contents, test_file)
