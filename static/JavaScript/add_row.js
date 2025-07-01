6780a1a13068d167096602343a55bbee14fae15e
kwestra <kk.westra@student.han.nl>
2025-07-01 13:38:30 +0200

    Added a temp css. Will be improved upon.

--- static/JavaScript/add_row.js
+++ static/JavaScript/add_row.js
@@ -4,9 +4,11 @@ const row = table.insertRow();
 
 const cell1 = row.insertCell(0);
 const cell2 = row.insertCell(1);
+const cell3 = row.insertCell(2);
 
 cell1.innerHTML = '<input type="text" name="terms" value="">';
 cell2.innerHTML = '<input type="text" name="defs" value="">';
+cell3.innerHTML = '<button type="button" onclick="removeRow(this)">Remove</button>';
 }
 
 function removeRow(button) {
