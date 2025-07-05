cec06235dd0f44f61119d07d4fbe29c60f926b2e
kwestra <kk.westra@student.han.nl>
2025-07-05 21:00:38 +0200

    Started on background to guess term / definitions.

---
+++ guessing_background.py
@@ -0,0 +1,32 @@
+from json_background import get_json
+import random
+def setup(file_contents):
+    content = {"correct":0,
+               "incorrect":0,
+               "fc":file_contents}
+    return content
+
+def correct(cont):
+    cont["correct"] += 1
+    return cont
+
+def incorrect(cont):
+    cont["incorrect"] += 1
+    return cont
+
+def get_rd(cont):
+    ter, defi = random.choice(list(cont["fc"].items()))
+    return ter, defi
+
+monkey = get_json("test_td")
+info = setup(monkey)
+
+def temp_set():
+    term, definition = get_rd(info)
+    meh = input(f"Guess for {term}: ")
+    if meh == definition:
+        print("Correct")
+    else:
+        print(f"Incorrect, {definition}")
+
+temp_set()
